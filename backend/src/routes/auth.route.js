84370b396b6cb0c23c578b1aff0f708e0d011c58
bedive <trungbed1ve@gmail.com>
2026-04-20 20:46:32 +0700

    refact(auth): refactor auth controller and auth route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,15 +1,4 @@
-import {
-    register,
-    login,
-    verifyEmail,
-    resendVerifyCode,
-    checkAuth,
-    logout,
-    oauthLogin,
-    resetPassword,
-    sendResetPasswordCode,
-    verifyResetCode
-} from "../controllers/auth.controller.js";
+import AuthController from "../controllers/auth.controller.js";
 
 import authMiddleware from "../middlewares/auth.middleware.js";
 import { validate } from "../middlewares/validate.middleware.js";
@@ -19,22 +8,24 @@ import { loginSchema } from "../validates/login.validate.js";
 import { oauthLoginRequest } from "../validates/oauthLogin.validate.js";
 import { verifyEmailRequest } from "../validates/verifyEmail.validate.js";
 import { reSendVerifyCodeRequest } from "../validates/resendVerifyEmail.validate.js";
-import { forgotPasswordRequest, verifyResetCodeRequest, resetPasswordRequest } from "../validates/forgotPassword.validate.js";
+import { forgotPasswordRequest, 
+        verifyResetCodeRequest, 
+        resetPasswordRequest } from "../validates/forgotPassword.validate.js";
 
 import { Router } from 'express';
 
 const route = Router();
 
-route.post('/register', validate(registerSchema), register);
-route.post('/login', validate(loginSchema), login);
-route.post('/verify', validate(verifyEmailRequest), verifyEmail);
-route.post('/verification-code/resend', validate(reSendVerifyCodeRequest), resendVerifyCode);
-route.post('/login/oauth', validate(oauthLoginRequest), oauthLogin);
-route.post('/forgot-password', validate(forgotPasswordRequest), sendResetPasswordCode);
-route.post("/verify-reset-code", validate(verifyResetCodeRequest), verifyResetCode);
-route.post('/reset-password', validate(resetPasswordRequest), resetPassword);
+route.post('/register', validate(registerSchema), AuthController.register);
+route.post('/login', validate(loginSchema), AuthController.login);
+route.post('/verify', validate(verifyEmailRequest), AuthController.verifyEmail);
+route.post('/verification-code/resend', validate(reSendVerifyCodeRequest), AuthController.resendVerifyCode);
+route.post('/login/oauth', validate(oauthLoginRequest), AuthController.oauthLogin);
+route.post('/forgot-password', validate(forgotPasswordRequest), AuthController.sendResetPasswordCode);
+route.post("/verify-reset-code", validate(verifyResetCodeRequest), AuthController.verifyResetCode);
+route.post('/reset-password', validate(resetPasswordRequest), AuthController.resetPassword);
 
-route.post('/refresh-token', authMiddleware.checkAuth, checkAuth);
-route.post('/logout', authMiddleware.auth, logout);
+route.post('/refresh-token', authMiddleware.checkAuth, AuthController.checkAuth);
+route.post('/logout', authMiddleware.auth, AuthController.logout);
 
 export default route;
